d6de43327efc9f21addf303fa1f4e26eb3b7f79f
root <root@debian.biandc.debian>
2022-11-30 15:48:19 +0800

    1

---
+++ src/t_062_101_symmetric_tree/isSymmetric.go
@@ -0,0 +1,19 @@
+package t_062_101_symmetric_tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return true
+}
